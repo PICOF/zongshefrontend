363b16f576af9cc11495297293103901dbb01dfa
PICOF <1821525505@qq.com>
2022-02-13 12:00:33 +0800

    fifth commit

--- js/frame.js
+++ js/frame.js
@@ -3,6 +3,7 @@ var side = document.querySelector('.sidebar');
 var sele = document.querySelector('.selector');
 var home = document.querySelector('.home');
 var view = document.querySelector('.view');
+var Lbox = document.querySelector('.lang ul');
 function changeH(){
     sele.style.top='141px';
     home.style.backgroundPosition='-150px -136px';
@@ -12,6 +13,16 @@ function changeV(){
     sele.style.top='191px';
     home.style.backgroundPosition='-282px -136px';
     view.style.backgroundPosition='-145px 0';
+}
+function changeL(){
+    Lbox.style.height='95px';
+}
+function changeCN(){
+    Lbox.style.height='0';
+}
+function changeEN(){
+    alert('勾罕见是吧');
+    Lbox.style.height='0';
 }
 function changeW(){
     show.style.height = window.innerHeight + 'px';
